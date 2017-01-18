ffcc72afce7dc4185147336603cf049de7f2895a
SmoDav <smodavprivate@gmail.com>
2017-01-18 11:46:53 +0300

    adding database laravel

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => getDatabase(),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
--- modules/index.php
+++ modules/index.php
@@ -1,5 +1,25 @@
 <?php
 
+function getDatabase()
+{
+
+    if (! isset($_SERVER['HTTP_HOST'])) {
+        return 'teamkazi_demo';
+    }
+    $pieces = explode('.', $_SERVER['HTTP_HOST']);
+
+    if (count($pieces) < 3) {
+        return 'teamkazi_demo';
+    }
+
+    if (strtolower($pieces[0]) == 'www') {
+        return 'teamkazi_demo';
+    }
+
+    return 'teamkazi_' . $pieces[0];
+}
+
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
